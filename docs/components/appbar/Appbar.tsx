605b1444361dc07ce41912654ddecd295f13715b
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-07 20:45:46 +0100

    update-in-docs

---
+++ docs/components/appbar/Appbar.tsx
@@ -0,0 +1,8 @@
+export default function Appbar(){
+    return(
+        <div className="w-full h-[8%] bg-black flex justify-center items-center p-3">
+            <div className="w-[20%] h-full bg-red-900"></div>
+            <div className="w-[80%] h-full bg-blue-900"></div>
+        </div>
+    );
+};
