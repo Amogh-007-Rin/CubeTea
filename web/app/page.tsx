c08886640e8f32c5727ac18c4381fdc7a40a18ca
Amogh-007-Rin <amoghdath233@gmail.com>
2026-07-07 13:28:44 +0100

    ported-to-web

---
+++ web/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Appbar from "../components/appbar/Appbar";
+
+export default function Home() {
+  return (
+    <div className="w-screen h-screen bg-zinc-900">
+      <Appbar></Appbar>
+    </div>
+   );
+}
---
+++ web/components/appbar/Appbar.tsx
@@ -0,0 +1,14 @@
+export default function Appbar(){
+    return(
+        <div className="w-full h-[8%] bg-black flex">
+            <div className="w-[20%] bg-green-300 h-full">
+
+            </div>
+
+            <div className="w-[80%] bg-red-300 h-full">
+
+            </div>
+        </div>
+    );
+
+}
